776372f43dc6a2125be8a9f84ea1b386084fd3e0
shalini <shalinijha812@gmail.com>
2018-12-08 16:47:25 +0530

    first commit

---
+++ src/main/java/com/example/AddSubb/AddSubbApplication.java
@@ -0,0 +1,12 @@
+package com.example.AddSubb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddSubbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddSubbApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/AddSubb/Calculation.java
@@ -0,0 +1,10 @@
+package com.example.AddSubb;
+
+public class Calculation {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
